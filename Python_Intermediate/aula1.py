9985b36266bd063f2652a5b3f5bc883dd90e415e
Lucas2155 <lucaslinhares980@gmail.com>
2022-12-07 21:22:32 -0300

    Add files

--- Python_Intermediate/aula1.py
+++ Python_Intermediate/aula1.py
@@ -1,1 +1,26 @@
-# Teste
+"""
+Introdução às funções (def) em Python
+Funções são trechos de códigos usados para
+replicar determinada ação ao longo do seu código.
+Elas podem receber valores para parâmetros (argumentos)
+e retornar um valor específico.
+Por padrão, funções Python retornam None (nada).
+"""
+def Print():
+    print('Várias')
+
+Print()
+
+def imprimir(a, b, c):
+    print(a, b, c)
+
+
+imprimir(1 , 2, 3)
+
+def saudacao(nome= 'Sem nome'): # Caso não passe um valor no argumento ele irá por default atribuir este valor
+    print(f'Olá {nome}!')
+
+saudacao('Lucas')
+saudacao('Maria')
+saudacao('Roberto')
+saudacao()
